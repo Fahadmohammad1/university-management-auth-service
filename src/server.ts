9a50eda5a222879dd176eb8069196c97b0f7121c
Fahad <205fahadmohammad@gmail.com>
2023-05-30 22:15:49 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("DB connected");
+    await mongoose.connect(config.database_url as string)
+    console.log('DB connected')
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("Failed to connect", error);
+    console.log('Failed to connect', error)
   }
 }
 
-main();
+main()
