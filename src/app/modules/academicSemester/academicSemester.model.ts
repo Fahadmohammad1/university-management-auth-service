08d12462ba8fe3b79d9341c3d545f9050d2f966a
Fahad <205fahadmohammad@gmail.com>
2023-06-11 20:24:41 +0600

    sortBy and sortOrder working successfully

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -11,32 +11,40 @@ import {
 import httpStatus from 'http-status'
 import ApiError from '../../../errors/ApiError'
 
-const academicSemesterSchema = new Schema<IAcademicSemester>({
-  title: {
-    type: String,
-    required: true,
-    enum: academicSemesterTitles,
+const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: {
+      type: String,
+      required: true,
+      enum: academicSemesterTitles,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: academicSemesterCodes,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
   },
-  year: {
-    type: Number,
-    required: true,
-  },
-  code: {
-    type: String,
-    required: true,
-    enum: academicSemesterCodes,
-  },
-  startMonth: {
-    type: String,
-    required: true,
-    enum: academicSemesterMonths,
-  },
-  endMonth: {
-    type: String,
-    required: true,
-    enum: academicSemesterMonths,
-  },
-})
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+)
 
 academicSemesterSchema.pre('save', async function (next) {
   const isExist = await AcademicSemester.findOne({
