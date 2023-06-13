65cbc72da04b9135c7d1db238e64a76a07f2fe7e
Fahad <205fahadmohammad@gmail.com>
2023-06-13 19:20:46 +0600

    academic department completed

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import { UserRoutes } from '../modules/user/user.route'
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route'
 
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route'
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route'
+
 const router = express.Router()
 
 const moduleRoutes = [
@@ -12,6 +15,14 @@ const moduleRoutes = [
   {
     path: '/academic-semester',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: academicDepartmentRoutes,
   },
 ]
 
