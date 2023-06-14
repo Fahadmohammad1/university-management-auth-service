fa22921a7889c1f2f04b1b4feff45ec2c686527d
Fahad <205fahadmohammad@gmail.com>
2023-06-15 03:57:56 +0600

    generate adminId is working successfully

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -23,6 +23,7 @@ export const generateStudentId = async (
   return incrementedId
 }
 
+// faculty id
 export const findLastFacultyId = async (): Promise<string | undefined> => {
   const lastFaculty = await User.findOne({ role: 'faculty' }, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
@@ -37,6 +38,24 @@ export const generateFacultyId = async (): Promise<string> => {
 
   let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
   incrementedId = `F-${incrementedId}`
+
+  return incrementedId
+}
+
+// admin id
+export const findLastAdminId = async (): Promise<string | undefined> => {
+  const lastAdmin = await User.findOne({ role: 'admin' }, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean()
+
+  return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined
+}
+
+export const generateAdminId = async (): Promise<string> => {
+  const currentId = (await findLastAdminId()) || (0).toString().padStart(5, '0')
+
+  let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  incrementedId = `A-${incrementedId}`
 
   return incrementedId
 }
